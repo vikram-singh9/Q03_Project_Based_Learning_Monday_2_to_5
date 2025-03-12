a92dbb251b2106715431393603ca98445fe14532
vikram-singh9 <163000375+vikram-singh9@users.noreply.github.com>
2025-03-12 11:49:46 +0500

    updated

---
+++ projects/money-making-machine/main.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import random
+import requests
+import time
+st.title("Money Making Machine")
+
+def generate_money ():
+    return random.randint(1,10000)
+st.subheader("Instant Money Generating Machine")
+if st.button("Generate Money"):
+    st.write("Counting Money...")
+    time.sleep(1)
+    amount = generate_money()
+    st.success(f"You made ${amount} today!😍")
+
