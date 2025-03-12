86a15298c80bbd4b26af0db91ac4bd58555d264d
vikram-singh9 <163000375+vikram-singh9@users.noreply.github.com>
2025-03-12 12:17:08 +0500

    Update main.py

--- projects/money-making-machine/main.py
+++ projects/money-making-machine/main.py
@@ -13,3 +13,15 @@ if st.button("Generate Money"):
     amount = generate_money()
     st.success(f"You made ${amount} today!😍")
 
+
+def fetch_side_hutles():
+    try:
+        response = requests.get("http://127.0.0.1:8000/side_hustles")
+        if response.status_code ==200:
+            hustles = response.json()
+            return hustles
+        else:
+            return st.w
+    except:
+        return st.write("Something went wrong")
+
